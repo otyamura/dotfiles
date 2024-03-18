5a1a3e4c5c12684b9c55fe9f8c06158a4b461c69
otyamura <suzuki08suzuki@gmail.com>
2024-03-18 23:13:11 +0900

    add install

--- dotfiles.sh
+++ dotfiles.sh
@@ -1,5 +1,8 @@
 #!/bin/sh
 
+git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
+git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+
 ln -sf ~/dotfiles/.vimrc ~/.vimrc
 mkdir -p ~/.vim/colors
 ln -sf ~/dotfiles/colors/kuroi.vim ~/.vim/colors/kuroi.vim
